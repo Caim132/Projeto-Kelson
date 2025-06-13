ece13cc49d906be6098bef5814754a3272fe5ab2
Vinicius Abrantes <127121589+Caim132@users.noreply.github.com>
2025-06-13 14:03:31 -0300

    Add files via upload

---
+++ src/services/CursoService.js
@@ -0,0 +1,30 @@
+import { addDoc, collection, getDocs, updateDoc, doc, deleteDoc, query, where } from 'firebase/firestore';
+import { db } from '../config/firebaseConfig';
+
+const cursosRef = collection(db, "cursos");
+
+export const getCursos = async (userId) => {
+    const q = query(cursosRef, where('userId', '==', userId));
+    const snapshot = await getDocs(q);
+    return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+};
+
+export const adicionarCurso =  async (curso, userId) => {
+    const docRef = await addDoc(cursosRef,
+        {
+            ...curso,
+            userId
+        }
+     );
+    return docRef.id;
+};
+
+export const atualizarCurso = async (id, novosDados) => {
+    const cursoRef = doc(db, 'cursos', id);
+    await updateDoc(cursoRef, novosDados);
+};
+
+export const deletarCurso = async (id) => {
+    const cursoRef = doc(db, 'cursos', id);
+    await deleteDoc(cursoRef);
+};
